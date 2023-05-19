df61f599d6538783fa76bfe665c233a49e764d22
Jaqueline <jaquelinenacarate@gmail.com>
2023-05-19 03:04:17 +0000

    Create loadTweets
    Create loadTweets to use jQuery to make a request to /tweets and receive the array of tweets as JSON. Updated the POST request to call loadTweets upon success. Calling loadTweets at the bottom of the file to render all existing tweets first.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
+// const data = [
+//   {
+//     user: {
+//       name: "Newton",
+//       avatars: "https://i.imgur.com/73hZDYK.png",
+//       handle: "@SirIsaac",
+//     },
+//     content: {
+//       text: "If I have seen further it is by standing on the shoulders of giants",
+//     },
+//     created_at: 1461116232227,
+//   },
+//   {
+//     user: {
+//       name: "Descartes",
+//       avatars: "https://i.imgur.com/nlhLi3I.png",
+//       handle: "@rd",
+//     },
+//     content: {
+//       text: "Je pense , donc je suis",
+//     },
+//     created_at: 1461113959088,
+//   },
+// ];
 
 
 // Ensure the web page is loaded:
@@ -39,24 +39,39 @@ $(document).ready(() => {
 
     const formData = $(this).serialize();
 
-     $.ajax({
-       method: "POST",
-       url: "/tweets",
-       data: formData,
-       success: function (res) {
-         // Handle the success response from the server
-         console.log("Tweet submitted successfully:", res);
-       },
-       // If an error occurred, log the issue
-       error: (error) => {
-         console.error(
-           `Error Encountered: ${error.status} - ${error.statusText}`
-         );
-       },
-     });
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: formData,
+      success: function (res) {
+        // Render new tweet
+        loadTweets();
+      },
+      // If an error occurred, log the issue
+      error: function (error) {
+        console.error(
+          `Error Encountered: ${error.status} - ${error.statusText}`
+        );
+      },
+    });
   });
 
- 
+  const loadTweets = function () {
+    $.ajax({
+      method: "GET",
+      url: "http://localhost:8080/tweets",
+      dataType: "json",
+      success: function (res) {
+        renderTweets(res);
+      },
+      // If an error occurred, log the issue
+      error: function (error) {
+        console.error(
+          `Error Encountered: ${error.status} - ${error.statusText}`
+        );
+      },
+    });
+  };
 
   // Takes in an array of tweets objects and appends each one to the #tweets-container
   const renderTweets = function(tweets) {
@@ -90,5 +105,6 @@ $(document).ready(() => {
     return $tweet;
   };
 
-  renderTweets(data);
+  // Render all tweets
+  loadTweets();
 })
